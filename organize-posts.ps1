9e484095d4659376ddd46b6634586a34e6b6f337
Koki Nakamura <nk.levia22@gmail.com>
2019-09-20 17:47:32 +1000

    Changed script to commit and push articles when posting an article.
    Changed script to commit and push articles when posting an article.

--- organize-posts.ps1
+++ organize-posts.ps1
@@ -24,6 +24,19 @@ foreach ($file in $fileList) {
     }
 }
 
+# Add changes to git.
+git add .
+
+# Create a commit message.
+$dateTime = Get-Date -format "yyyy-MM-dd HH:mm:ss K"
+$commitMsg = "Posts organized: ${dateTime}"
+
+# Commit.
+git commit -m $commitMsg
+
+# Push posts and build repos.
+git push origin master
+
 # Come back to the project root.
 Set-Location .\..\..
 
