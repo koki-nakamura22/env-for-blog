bb9d487c59567bea28c801525aac3bf24af50fa2
Koki Nakamura <nk.levia22@gmail.com>
2019-10-02 22:00:44 +1000

    Added a function of add a title to code block.
    Added a function of add a title to code block.

--- static/js/add-on.js
+++ static/js/add-on.js
@@ -4,3 +4,19 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+
+(function addTitleToCodeBlock() {
+  let list = document.body.getElementsByClassName("highlight");
+
+  for (i = 0; i <= list.length - 1; i++) {
+    let code = list[i].firstElementChild.firstElementChild;
+    let codeName = code ? code.className.split(":")[1] : null;
+
+    if (codeName) {
+      let div = document.createElement("div");
+      div.textContent = codeName;
+      div.classList.add("code-name");
+      code.parentNode.insertBefore(div, code);
+    }
+  }
+}());
